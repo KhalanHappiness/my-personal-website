6aaefe1ba11a3f4891a63f35e0aaec5a589354a9
happiness <happinesskhalan@gmail.com>
2025-03-03 12:03:48 +0300

    adds javascript to the page

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("the page has been loaded")
+
+    document.getElementById("resumebtn").addEventListener('click',function(){
+        window.open('Assets/Happiness Mwita Khalan CV.pdf (1).pdf', '_blank')
+    });
+
+    document.getElementById("resumedwnld").addEventListener('click',function(){
+        window.open('Assets/Happiness Mwita Khalan CV.pdf (1).pdf', '_blank')
+    });
+});
