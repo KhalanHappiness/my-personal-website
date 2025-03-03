e20c95bc4d6c084c2dc4c16319538a3ea733ff3f
happiness <happinesskhalan@gmail.com>
2025-03-03 15:13:45 +0300

    Adjust the download resume button and adds an alert

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
         window.open('Assets/Happiness Mwita Khalan CV.pdf (1).pdf', '_blank')
     });
 
+    function showDownloadAlert(){
+        alert("resume has been downloaded")
+    }
+
     document.getElementById("resumedwnld").addEventListener('click',function(){
-        window.open('Assets/Happiness Mwita Khalan CV.pdf (1).pdf', '_blank')
+        const link = document.createElement('a');
+        link.href ='Assets/Happiness Mwita Khalan CV.pdf (1).pdf'
+        link.download = 'Assets/Happiness Mwita Khalan CV.pdf (1).pdf'
+
+        document.body.appendChild(link)
+
+        link.click()
+
+        document.body.removeChild(link)
+
+        showDownloadAlert();
     });
 });
